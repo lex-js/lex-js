00c42ee66f9926c0015954177c30f2df352625e3
Marise Hayashi <vyatheslav2707@mail.ru>
2019-03-21 01:31:31 +0300

    Job name

--- scripts/preparePkg.sh
+++ scripts/preparePkg.sh
@@ -12,11 +12,11 @@ for (( i=0; i < ${#dirs[@]}; i+=1 )); do
   cp -R files "${dirs[$i]}"
 done
 
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x86.exe -o build/lexdist-win-x86/lex-win-x86.exe
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x64.exe -o build/lexdist-win-x64/lex-win-x64.exe
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-linux-x64 -o build/lexdist-linux-x64/lex-linux-x64
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-linux-x86 -o build/lexdist-linux-x86/lex-linux-x86
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-macos-x64 -o build/lexdist-macos-x64/lex-macos-x64
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x86.exe?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DVisual%20Studio%202015%2C%20PYTHON%3DC%3A%5CPython34%2C%20ARCH%3Dx86 -o build/lexdist-win-x86/lex-win-x86.exe
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x64.exe?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DVisual%20Studio%202015%2C%20PYTHON%3DC%3A%5CPython34-x64%2C%20ARCH%3Dx64 -o build/lexdist-win-x64/lex-win-x64.exe
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-linux-x64?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DUbuntu1804 -o build/lexdist-linux-x64/lex-linux-x64
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-linux-x86?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DUbuntu1804 -o build/lexdist-linux-x86/lex-linux-x86
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-macos-x64?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DUbuntu1804 -o build/lexdist-macos-x64/lex-macos-x64
 
 echo build/lexdist-linux-x64/xdg-open build/lexdist-linux-x86/xdg-open | xargs -n 1 curl -sSL https://github.com/sindresorhus/opn/raw/master/xdg-open -o $1
 
