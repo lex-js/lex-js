a7a7b45d219cfefaffcd8d9dd92a0b211cd9f4d0
Marise Hayashi <vyatheslav2707@mail.ru>
2019-03-21 09:54:38 +0300

    move config

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const externalCwd = process.pkg ? path.dirname(process.execPath) : __dirname;
 const internalCwd = process.pkg
   ? path.dirname(process.pkg.entrypoint)
   : __dirname;
-const configPath = path.join(internalCwd, "server", "config-server.json");
-const schemaPath = path.join(internalCwd, "server", "config-server-schema.json");
+const configPath = path.join(internalCwd, "config", "config-server.json");
+const schemaPath = path.join(internalCwd, "config", "config-server-schema.json");
 const config = require(configPath);
 const schema = require(schemaPath);
 
