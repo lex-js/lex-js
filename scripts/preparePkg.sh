8b4fe4c67e9585fb27a9e941c80254db5dcb2438
8084 <klntsky@gmail.com>
2019-03-29 21:54:10 +0300

    added: basic release archive

--- scripts/preparePkg.sh
+++ scripts/preparePkg.sh
@@ -1,6 +1,6 @@
-mkdir -p build/{lexdist-win-x64,lexdist-win-x86}
+mkdir -p build/{lex-js-win-x64,lex-js-win-x86}
 
-readarray -d '' dirs < <(find build -type d -iname "lexdist-*" -print0)
+readarray -d '' dirs < <(find build -type d -iname "lex-js-*" -print0)
 IFS=$'\n'
 dirs=($(sort <<<"${dirs[*]}"))
 
@@ -8,8 +8,8 @@ for (( i=0; i < ${#dirs[@]}; i+=1 )); do
   cp -R config "${dirs[$i]}"
 done
 
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x86.exe?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DVisual%20Studio%202015%2C%20PYTHON%3DC%3A%5CPython34%2C%20ARCH%3Dx86 -o build/lexdist-win-x86/lex-win-x86.exe
-curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x64.exe?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DVisual%20Studio%202015%2C%20PYTHON%3DC%3A%5CPython34-x64%2C%20ARCH%3Dx64 -o build/lexdist-win-x64/lex-win-x64.exe
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x86.exe?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DVisual%20Studio%202015%2C%20PYTHON%3DC%3A%5CPython34%2C%20ARCH%3Dx86 -o build/lex-js-win-x86/lex-win-x86.exe
+curl -SL https://ci.appveyor.com/api/projects/limitedeternity/lex-js/artifacts/build/lex-win-x64.exe?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DVisual%20Studio%202015%2C%20PYTHON%3DC%3A%5CPython34-x64%2C%20ARCH%3Dx64 -o build/lex-js-win-x64/lex-win-x64.exe
 
 cd build/
 for (( i=0; i < ${#dirs[@]}; i+=1 )); do
@@ -18,3 +18,5 @@ for (( i=0; i < ${#dirs[@]}; i+=1 )); do
 done
 
 cd ../
+
+zip -r -FS ./build/lex-js-basic.zip index.html public
