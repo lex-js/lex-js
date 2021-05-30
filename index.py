bc8f61bfc76d136f560331d4d96aa20d9afcadf4
Vyacheslav Bespalov <vyatheslav2707@mail.ru>
2021-05-30 15:40:59 +0300

    Fix content browser bug, npm audit, fix build

--- index.py
+++ index.py
@@ -9,23 +9,21 @@ from glob import glob as find_by_glob
 from bottle import get, request, static_file, HTTPResponse, run
 from json import dumps as json_to_string
 
-RUNTIME = {
-    "external_root": None,
-    "internal_root": None,
-    "tab_url": None,
-}
 
 CONFIG = {
     "port": 64320,
 }
 
-if getattr(sys, 'frozen', False):
-    RUNTIME["external_root"] = abspath(dirname(sys.executable))
-    RUNTIME["internal_root"] = sys._MEIPASS
+RUNTIME = {
+    "external_root": abspath(dirname(__file__)),
+    "internal_root": abspath(dirname(__file__)),
+    "tab_url": "http://localhost:{0}".format(CONFIG["port"]),
+}
 
-else:
-    RUNTIME["internal_root"] = RUNTIME["external_root"] = abspath(
-        dirname(__file__))
+
+if getattr(sys, "frozen", False):
+    RUNTIME["external_root"] = abspath(dirname(sys.executable))
+    RUNTIME["internal_root"] = getattr(sys, "_MEIPASS")
 
 if len(sys.argv) > 1:
     print("Opening file {0}".format(basename(abspath(sys.argv[1]))))
@@ -34,65 +32,70 @@ if len(sys.argv) > 1:
         CONFIG["port"],
         abspath(sys.argv[1]).replace('\\', '/')
     )
-else:
-    RUNTIME["tab_url"] = "http://localhost:{0}".format(
-        CONFIG["port"])
 
 
-@get('/')
+@get("/")
 def index():
-    return static_file('index.html', root=RUNTIME["internal_root"])
+    return static_file("index.html", root=RUNTIME["internal_root"])
 
 
-@get('/api')
+@get("/api")
 def api():
-    action = request.query.action
+    try:
+        action = getattr(request.query, "action")
 
-    if action == "listdir":
-        path = request.query.dir
+        if action == "listdir":
+            path = getattr(request.query, "dir").replace("\\", path_separator) \
+                if path_separator == "/" \
+                else getattr(request.query, "dir").replace("/", path_separator)
 
-        listing = sorted(find_by_glob("{0}{1}*".format(path, path_separator)))
+            listing = sorted(
+                find_by_glob("{0}{1}*".format(
+                    path,
+                    path_separator if not path.endswith(path_separator) else ""
+                ))
+            )
 
-        dir_list = []
-        file_list = []
+            dir_list = []
+            file_list = []
 
-        for entry in listing:
-            try:
-                stat_info = file_props(entry).stat()
-            except BaseException:
-                continue
+            for entry in listing:
+                try:
+                    stat_info = file_props(entry).stat()
 
-            obj = {
-                "name": basename(entry),
-                "modified": stat_info.st_mtime * 1000
-            }
+                except OSError:
+                    continue
 
-            if file_props(entry).is_file():
-                obj["type"] = "file"
-                obj["size"] = stat_info.st_size
-                file_list.append(obj)
+                obj = {
+                    "name": basename(entry),
+                    "modified": stat_info.st_mtime * 1000
+                }
 
-            else:
-                obj["type"] = "directory"
-                dir_list.append(obj)
+                if file_props(entry).is_file():
+                    obj["type"] = "file"
+                    obj["size"] = stat_info.st_size
+                    file_list.append(obj)
 
-        response = HTTPResponse(status=200)
-        response.body = json_to_string(dir_list + file_list)
-        response.set_header(
-            "Cache-Control", "no-cache, no-store, must-revalidate")
-        response.set_header("Pragma", "no-cache")
-        response.set_header("Expires", "0")
-        return response
+                else:
+                    obj["type"] = "directory"
+                    dir_list.append(obj)
 
-    elif action == "getfile":
-        fname = abspath(request.query.file)
-
-        if not file_props(fname).is_file():
-            return HTTPResponse(status=404)
-
-        try:
             response = HTTPResponse(status=200)
-            with open_file(fname, 'rb') as fd:
+            response.body = json_to_string(dir_list + file_list)
+            response.set_header(
+                "Cache-Control", "no-cache, no-store, must-revalidate")
+            response.set_header("Pragma", "no-cache")
+            response.set_header("Expires", "0")
+            return response
+
+        elif action == "getfile":
+            fname = abspath(getattr(request.query, "file"))
+
+            if not file_props(fname).is_file():
+                return HTTPResponse(status=404)
+
+            response = HTTPResponse(status=200)
+            with open_file(fname, "rb") as fd:
                 response.body = fd.read()
 
             response.set_header(
@@ -100,15 +103,15 @@ def api():
             response.set_header("Pragma", "no-cache")
             response.set_header("Expires", "0")
             return response
-        except BaseException:
-            pass
 
-        return HTTPResponse(status=403)
+        else:
+            raise AttributeError("No such action")
 
-    return HTTPResponse(status=400)
+    except AttributeError:
+        return HTTPResponse(status=400)
 
 
-@get('/public/<filename:path>')
+@get("/public/<filename:path>")
 def send_static(filename):
     return static_file(filename, root=join_path(RUNTIME["internal_root"], "public"))
 
@@ -123,9 +126,6 @@ class Call:
         return self.fn(*self.args, **self.kwargs)
 
 
-try:
-    Timer(0.5, Call(print, "Listening on http://localhost:" + str(CONFIG["port"]))).start()
-    Timer(1, Call(open_browser_tab, RUNTIME["tab_url"])).start()
-    run(host='localhost', port=CONFIG["port"], quiet=True, debug=False)
-except BaseException:
-    pass
+Timer(0.5, Call(print, "Listening on http://localhost:{0}".format(CONFIG["port"]))).start()
+Timer(1, Call(open_browser_tab, RUNTIME["tab_url"])).start()
+run(host="localhost", port=CONFIG["port"], quiet=True, debug=False)
